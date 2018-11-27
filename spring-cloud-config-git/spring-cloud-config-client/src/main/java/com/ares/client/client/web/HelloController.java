408966eef4f828e17db74bcf90730730940aac9c
wangjian <wangjianhl@cmhi.chinamobile.com>
2018-11-27 14:22:46 +0800

    client

---
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/ares/client/client/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.ares.client.client.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: wj
+ * @Date: 2018/11/27 14:19
+ * @Description:
+ */
+@RestController
+public class HelloController {
+    @Value("${ares.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from(){
+        return this.hello;
+    }
+
+}
