11582631eff2ae28f5bdb7046b08b765ad9d35fa
wangjian <wangjianhl@cmhi.chinamobile.com>
2018-11-28 10:52:46 +0800

    change dev

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/ares/client/client/web/HelloController.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/ares/client/client/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.ares.client.client.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
-public class HelloController {
+ class HelloController {
     @Value("${ares.hello}")
     private String hello;
 
